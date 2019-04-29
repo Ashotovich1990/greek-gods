414d21ff67501d0751ca5b8ddb850627af93b2d9
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-04-29 14:27:23 -0700

    Add abode and emblem to GraphQL root query

--- server/schema/root_query.js
+++ server/schema/root_query.js
@@ -5,6 +5,13 @@ const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
 const GodType = require('./god_type');
 const God = mongoose.model('god');
 
+const EmblemType = require('./emblem_type');
+const Emblem = mongoose.model('emblem'); 
+
+const AbodeType = require('./abode_type');
+const Abode = mongoose.model('abode');
+
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: () => ({
@@ -19,6 +26,34 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: new GraphQLNonNull(GraphQLID) } },
             resolve(parentValue, { id }) {
                 return God.findById(id);
+            }
+        },
+
+        emblems: {
+            type: new GraphQLList(EmblemType),
+            resolve() {
+                return Emblem.find({});
+            }
+        }, 
+        emblem: {
+            type: EmblemType,
+            args: { id: { type: new GraphQLNonNull(GraphQLID)} },
+            resolve(parentValue, { id }) {
+                return Emblem.findById(id);
+            }
+        }, 
+
+        abodes: {
+            type: new GraphQLList(AbodeType), 
+            resolve() {
+                return Abode.find({});
+            }
+        }, 
+        abode: {
+            type: AbodeType,
+            args: { id: { type: new GraphQLNonNull(GraphQLID)}},
+            resolve(parentValue, { id }) {
+                return Abode.findById(id);
             }
         }
     })
